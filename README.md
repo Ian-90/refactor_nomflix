dbf9343ef6eeb459ae430db56ddaef265a8b2e05
LeeYoHan <yohan4551@gmail.com>
2021-07-30 23:11:03 +0900

    feat: init repo

---
+++ README.md
@@ -0,0 +1,3 @@
+# Nomflix Refactoring
+* [Nomflix](https://academy.nomadcoders.co/p/react-for-beginners)
+  * 수업들은거 hook 적용해보기
---
+++ src/Routes/Detail/DetailContainer.js
@@ -0,0 +1,60 @@
+import React from "react";
+import DetailPresenter from './DetailPresenter';
+import { moviesApi, tvApi } from "../../api";
+
+export default class extends React.Component {
+  constructor(props) {
+    super(props);
+    const { 
+      location: { pathname } 
+    } = props;
+    this.state = {
+      result: null,
+      error: null,
+      loading: true,
+      isMovie: pathname.includes("/movie/")      
+    }
+  }
+
+  async componentDidMount() {
+    const {
+      match: {
+        params: { id }
+      },
+      history: { push }
+    } = this.props;
+    const { isMovie } = this.state;
+    const parseId = parseInt(id);
+    if (isNaN(parseId)) {
+      return push("/")
+    }
+    let result = null;
+    try {
+      if (isMovie) {
+        ({ data: result } = await moviesApi.movieDetail(parseId));
+      } else {
+        ({ data: result} = await tvApi.showDetail(parseId)); 
+      }
+    } catch {
+      this.setState({
+        error: "Can't find anything."
+      })
+    } finally {
+      this.setState({
+        loading: false,
+        result
+      })
+    }
+  }
+
+  render () {
+    const { result, error, loading} = this.state;
+    return (
+      <DetailPresenter
+        result={result}
+        error={error}
+        loading={loading}
+      />
+    )
+  }
+}
