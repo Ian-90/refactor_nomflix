08bce5f978b7a1568818af4930e4443dc59b8092
LeeYoHan <yohan4551@gmail.com>
2021-12-12 16:51:53 +0900

    feat: apply useState and useEffect in TVContainer

--- src/Routes/TV/index.js
+++ src/Routes/TV/index.js
@@ -1,3 +1,49 @@
-import TVContainer from './TVContainer';
+import React, { useState, useEffect } from "react";
+import TVPresenter from './TVPresenter';
+import { tvApi } from 'api';
 
-export default TVContainer;
+const TV = () => {
+  const [topRated, setTopRated] = useState(null)
+  const [popular, setPopular] = useState(null)
+  const [airingToday, setAiringToday] = useState(null)
+  const [error, setError] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+  const getTVData = async () => {
+    try {
+      const {
+        data: { results: topRated }
+      } = await tvApi.topRated();
+      const {
+        data: { results: popular }
+      } = await tvApi.popular();
+      const {
+        data: { results: airingToday }
+      } = await tvApi.airingToday();
+
+      setTopRated(topRated)
+      setPopular(popular)
+      setAiringToday(airingToday)
+    } catch {
+      setError("Can't find TV information.")
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    getTVData()
+  }, [])
+
+  return (
+    <TVPresenter
+      topRated={topRated}
+      popular={popular}
+      airingToday={airingToday}
+      error={error}
+      loading={loading}
+    />
+  )
+}
+
+export default TV
