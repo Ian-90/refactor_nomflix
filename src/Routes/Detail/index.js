0508bc14ce566a287abdc2f7b1e6c37cb81f270c
LeeYoHan <yohan4551@gmail.com>
2021-12-23 01:10:37 +0900

    feat: apply hook in DetailContainer component

--- src/Routes/Detail/index.js
+++ src/Routes/Detail/index.js
@@ -1,3 +1,48 @@
-import DetailContainer from './DetailContainer';
+import React, { useState, useEffect } from "react";
+import DetailPresenter from './DetailPresenter';
+import { moviesApi, tvApi } from "../../api";
 
-export default DetailContainer;
+const Detail = ({
+  location: { pathname },
+  match: { params: { id } },
+  history: { push }
+}) => {
+  const [result, setResult] = useState(null)
+  const [error, setError] = useState(null)
+  const [isLoading, setIsLoading] = useState(true)
+  const [isMovie, setIsMovie] = useState(pathname.includes('/movie/'))
+
+  const getDetailResults = async () => {
+    const parseId = parseInt(id);
+    if (isNaN(parseId)) {
+      return push("/")
+    }
+    let result = null;
+    try {
+      if (isMovie) {
+        ({ data: result } = await moviesApi.movieDetail(parseId));
+      } else {
+        ({ data: result} = await tvApi.showDetail(parseId));
+      }
+    } catch {
+      setError("Can't find anything.")
+    } finally {
+      setResult(result)
+      setIsLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    getDetailResults()
+  }, [])
+
+  return (
+    <DetailPresenter
+      result={result}
+      error={error}
+      loading={isLoading}
+    />
+  )
+}
+
+export default Detail
